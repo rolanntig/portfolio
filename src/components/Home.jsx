3254d04c531bc2cb2910e053aabe637cd4509f00
rolanntig <106159101+rolanntig@users.noreply.github.com>
2024-01-26 16:15:16 +0100

    removed jumpscare

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,41 +3,15 @@ import '../App.css'
 
 
 const Home = () => {
-  const [showJumpscare, setShowJumpscare] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowJumpscare(true);
-       const audio = new Audio("metal-pipe-clang.mp3");
-       audio.play();
-    }, 5000);
-
- 
 
 
 
-  }, []);
-
-     if (showJumpscare) {
-       setTimeout(() => {
-         setShowJumpscare(false);
-       }, 3000);
-     }
+  
 
 
 
   return (
     <div className="flex justify-center items-center h-full">
-      {showJumpscare && (
-        <div className="jumpscare-container">
-          <img
-            src={"IMG_0459.jpg"}
-            alt="Jumpscare"
-            className="jumpscare-image"
-          />
-        </div>
-      )}
-
       <div className="flex flex-col items-center">
         <h1 className=" flex text-6xl ">
           Hello I'm <p className="mx-3 text-orange-400">Rolan</p>
